2717fef935a83b03a4f41f99c665defcf024ebca
Sebastian Mellor <sebble@sebble.com>
2015-07-13 16:30:22 +0100

    things

--- app.ts
+++ app.ts
@@ -17,10 +17,22 @@ class MyAppComponent {
     name: string;
     names: Array<string>;
     todos: Array<string>;
+    version: string;
+    included: Array<boolean>;
   
     constructor() {
         this.name = 'Alice';
         this.names = ["Aarav", "MartÃ­n", "Shannon", "Ariana", "Kai"];
+        this.version = 'simple';
+        this.included = [true, true, false, true];
+    }
+
+    ver(v) {
+        this.version = v;
+    }
+
+    inc() {
+        
     }
 }
 
